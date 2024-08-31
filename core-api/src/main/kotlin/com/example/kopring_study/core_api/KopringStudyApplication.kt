1c2c907aed51b741a8de56632d514813d3748d4f
gk <iba3374@naver.com>
2024-08-31 20:53:15 +0900

    fix: ComponentScan 추가

--- core-api/src/main/kotlin/com/example/kopring_study/core_api/KopringStudyApplication.kt
+++ core-api/src/main/kotlin/com/example/kopring_study/core_api/KopringStudyApplication.kt
@@ -2,8 +2,10 @@ package com.example.kopring_study.core_api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["com.example.kopring_study"])
 class KopringStudyApplication
 
 fun main(args: Array<String>) {
