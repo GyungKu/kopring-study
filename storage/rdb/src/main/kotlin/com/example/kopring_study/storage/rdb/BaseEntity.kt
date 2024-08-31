85ad3bf6f33f70f4c00a33efd5b14eef2a53cb9a
gk <iba3374@naver.com>
2024-08-31 20:52:58 +0900

    fix: BaseEntity에서 id는 제거

--- storage/rdb/src/main/kotlin/com/example/kopring_study/storage/rdb/BaseEntity.kt
+++ storage/rdb/src/main/kotlin/com/example/kopring_study/storage/rdb/BaseEntity.kt
@@ -10,10 +10,6 @@ import java.time.ZonedDateTime
 
 @MappedSuperclass
 internal abstract class BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null
-
     @CreationTimestamp
     val createdAt: ZonedDateTime? = null
 
